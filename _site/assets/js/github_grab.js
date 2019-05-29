f5a03427ae569d11848b08d804ce6a723f1ecae6
Chris Vanderloo <chris.vanderloo@yahoo.com>
2019-05-29 18:57:07 -0400

    Added Dark Mode

--- _site/assets/js/github_grab.js
+++ _site/assets/js/github_grab.js
@@ -1,7 +1,16 @@
+document.addEventListener("DOMContentLoaded", (event) => {
+  if (localStorage.getItem("darkMode") == 1) {
+    body.classList.remove("lightbg");
+    body.classList.add("dark");
+  }
+});
+
 let height = () => window.innerHeight || e.clientHeight;
 let width = () => window.innerWidth || e.clientWidth;
 let getScroll = () => document.documentElement.scrollTop || document.body.scrollTop;
 let isHidden = (el) => el.classList.contains("hidden");
+
+var body = document.getElementsByTagName("BODY")[0];
 
 function checkBack() {
   let back = document.getElementById("backBtn");
@@ -30,5 +39,18 @@ function togglePageOpen(page) {
   }
   else {
     back.classList.remove("hidden");
+  }
+}
+
+function toggleDark() {
+  if (body.classList.contains("dark")) {
+    localStorage.setItem("darkMode", 0);
+    body.classList.remove("dark");
+    body.classList.add("lightbg");
+  }
+  else {
+    localStorage.setItem("darkMode", 1);
+    body.classList.remove("lightbg");
+    body.classList.add("dark");
   }
 }
