62f7bef91b8977e84e10a90921c4b6f4ab2f53ef
Chris Vanderloo <chris.vanderloo@yahoo.com>
2020-08-25 21:12:32 -0400

    updated homepage mp4 to compressed version

--- src/components/bulma/index.js
+++ src/components/bulma/index.js
@@ -7,3 +7,8 @@ export const Container = (props) => (<div {...props} className={c("container", p
 export const Hero = (props) => (<section {...props} className={c("hero", props.className)}>{props.children}</section>);
 export const ColumnContainer = (props) => (<div {...props} className={c("columns", props.className)}>{props.children}</div>);
 export const Column = (props) => (<div {...props} className={c("column", props.className)}>{props.children}</div>);
+export const Tile = (props) => (<div {...props} className={c("tile", props.className)}>{props.children}</div>);
+export const TileChild = (props) => (<Tile {...props} className={c("is-child", props.className)}>{props.children}</Tile>);
+export const TileAncestor = (props) => (<Tile {...props} className={c("is-ancestor", props.className)}>{props.children}</Tile>);
+export const TileParent = (props) => (<Tile {...props} className={c("is-parent", props.className)}>{props.children}</Tile>);
+
