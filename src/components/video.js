f6cc34dd794c23c9ee826f66914cb5ddff2b4a1f
Chris Vanderloo <chris.vanderloo@yahoo.com>
2020-08-25 21:20:33 -0400

    canplay attribute

--- src/components/video.js
+++ src/components/video.js
@@ -8,7 +8,7 @@ export default function Video({ source, loaded, style }) {
     loaded();
   }
   return (
-    <video style={style} ref={videoEl} onCanPlayThrough={onVideoLoad} loop playsInline inline autoPlay muted>
+    <video style={style} ref={videoEl} canPlay={onVideoLoad} loop playsInline inline autoPlay muted>
       <source src={source} type="video/mp4" />
     </video>
   );
