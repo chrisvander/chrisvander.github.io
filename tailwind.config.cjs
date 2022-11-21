ed1dae79abcb306dd82a357902f94ee6348bcca2
Christian van der Loo <chris.vanderloo@yahoo.com>
2022-11-21 14:08:03 -0500

    Adds tailwind stuff, sass, typography

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -9,5 +9,8 @@ module.exports = {
       },
     },
   },
-  plugins: [require("@tailwindcss/line-clamp")],
+  plugins: [
+    require("@tailwindcss/typography"),
+    require("@tailwindcss/line-clamp"),
+  ],
 };
