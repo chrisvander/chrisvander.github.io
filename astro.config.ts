4afb072687905d46867fe12a57db88bfce747778
Christian van der Loo <chris.vanderloo@icloud.com>
2025-05-03 20:55:41 -0400

    change tailwind config

--- astro.config.ts
+++ astro.config.ts
@@ -14,7 +14,10 @@ export default defineConfig({
 	},
 	markdown: {
     shikiConfig: {
-      theme: 'catppuccin-mocha',
+      themes: {
+        light: "catppuccin-latte",
+        dark: "catppuccin-mocha"
+      },
     },
   },
 });
