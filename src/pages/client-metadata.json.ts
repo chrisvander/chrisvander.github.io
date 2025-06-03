41e55432aed85548baeae56f65226c6cef483610
Christian van der Loo <chris.vanderloo@icloud.com>
2025-06-02 21:23:38 -0400

    add get endpoint

---
+++ src/pages/client-metadata.json.ts
@@ -0,0 +1,14 @@
+export async function GET() {
+  return {
+    client_id: "https://chrisvanderloo.com/client-metadata.json",
+    client_name: "Chris van der Loo's Blog",
+    client_uri: "https://chrisvanderloo.com",
+    redirect_uris: ["https://chrisvanderloo.com/callback"],
+    scope: "atproto",
+    grant_types: ["authorization_code", "refresh_token"],
+    response_types: ["code"],
+    token_endpoint_auth_method: "none",
+    application_type: "web",
+    dpop_bound_access_tokens: true,
+  }
+}
