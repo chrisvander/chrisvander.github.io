2193f5a66b2da117594b750722596eed6af03949
Christian van der Loo <chris.vanderloo@icloud.com>
2026-03-11 19:44:01 -0400

    add cloudflare

--- astro.config.ts
+++ astro.config.ts
@@ -3,21 +3,28 @@ import icon from "astro-icon";
 import tailwindcss from "@tailwindcss/vite";
 import { defineConfig } from "astro/config";
 
+import cloudflare from "@astrojs/cloudflare";
+
 export default defineConfig({
   site: "https://chrisvanderloo.com/",
-	integrations: [
-		icon(),
-		sitemap(),
+
+  integrations: [
+      icon(),
+      sitemap(),
 	],
-	vite: {
-    plugins: [tailwindcss() as any],
-  },
-	markdown: {
-    shikiConfig: {
-      themes: {
-        light: "catppuccin-latte",
-        dark: "catppuccin-mocha"
-      },
+
+  vite: {
+  plugins: [tailwindcss() as any],
+},
+
+  markdown: {
+  shikiConfig: {
+    themes: {
+      light: "catppuccin-latte",
+      dark: "catppuccin-mocha"
     },
   },
+},
+
+  adapter: cloudflare(),
 });
