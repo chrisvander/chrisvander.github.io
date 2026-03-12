33e23907abaed82c61b3032eb001b2445d4b4843
Christian van der Loo <chris.vanderloo@icloud.com>
2026-03-11 20:05:12 -0400

    fix cloudflare setup

--- astro.config.ts
+++ astro.config.ts
@@ -2,29 +2,24 @@ import sitemap from "@astrojs/sitemap";
 import icon from "astro-icon";
 import tailwindcss from "@tailwindcss/vite";
 import { defineConfig } from "astro/config";
-
 import cloudflare from "@astrojs/cloudflare";
 
 export default defineConfig({
   site: "https://chrisvanderloo.com/",
-
   integrations: [
       icon(),
       sitemap(),
 	],
-
   vite: {
-  plugins: [tailwindcss() as any],
-},
-
+    plugins: [tailwindcss() as any],
+  },
   markdown: {
-  shikiConfig: {
-    themes: {
-      light: "catppuccin-latte",
-      dark: "catppuccin-mocha"
+    shikiConfig: {
+      themes: {
+        light: "catppuccin-latte",
+        dark: "catppuccin-mocha"
+      },
     },
   },
-},
-
   adapter: cloudflare(),
 });
